625892870f33f4bf1196f50ef94788101f0eab8c
bencelaszlo <bencelaszlo@protonmail.com>
2021-10-18 20:07:27 +0200

    Refactor Book as a functional component, fix shelf selecting

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,28 +1,30 @@
 import React from 'react'
 
-class Book extends React.Component {
-    render() {
-        return (
-            <div className="book">
-                <div className="book-top">
-                {this.props.imageLinks && this.props.imageLinks.thumbnail &&
-                    <div className="book-cover" style={{ width: 200, height: 200, backgroundImage: `url(${this.props.imageLinks.thumbnail})` }}></div>
-                }
-                <div className="book-shelf-changer">
-                    <select defaultValue={this.props.shelf ? this.props.shelf : "none"}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading" onClick={() => this.props.updateBook(this.props.id, 'currentlyReading')}>Currently Reading</option>
-                    <option value="wantToRead" onClick={() => this.props.updateBook(this.props.id, 'wantToRead')}>Want to Read</option>
-                    <option value="read" onClick={() => this.props.updateBook(this.props.id, 'read')}>Read</option>
-                    <option value="none" onClick={() => this.props.updateBook(this.props.id, 'none')}>None</option>
-                    </select>
-                </div>
-                </div>
-                <div className="book-title">{this.props.title}</div>
-                <div className="book-authors">{this.props.authors}</div>
+const Book = ({ id, imageLinks, shelf, title, authors, updateBook }) => {
+    const changeShelf = (shelf) => {
+        updateBook(id, shelf);
+    };
+
+   return (
+        <div className="book">
+            <div className="book-top">
+            {imageLinks && imageLinks.thumbnail &&
+                <div className="book-cover" style={{ width: 200, height: 200, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+            }
+            <div className="book-shelf-changer">
+                <select value={shelf ? shelf : "none"} onChange={event => changeShelf(event.target.value)}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+                </select>
             </div>
-        )
-    }
+        </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors.join(', ')}</div>
+        </div>
+   );
 }
 
-export default Book
+export default Book;
